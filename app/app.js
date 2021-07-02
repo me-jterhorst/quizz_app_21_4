a729bcf7dde90312a6f4aca5ce34a7e3646f7b62
Jakob <me@jterhorst.de>
2021-07-02 16:04:59 +0200

    Add JavaScript, Add Input Fields

--- app/app.js
+++ app/app.js
@@ -2,6 +2,11 @@ const BOOKMARKS = Array.from(document.querySelectorAll(".bookmark"));
 const SHOW_ANSWER_BTN = Array.from(
   document.querySelectorAll(".content-card__btn")
 );
+const DARK_TOGGLE = document.querySelector(".dark-mode-toggle");
+const LIGHT_BULB = document.querySelector(".light");
+const FORM = document.querySelector("form");
+
+let formArray = [];
 
 BOOKMARKS.forEach((bookmark) => {
   bookmark.addEventListener("click", (event) => {
@@ -22,4 +27,53 @@ SHOW_ANSWER_BTN.forEach((answer_btn) => {
       ANSWER.classList.add("hidden");
     }
   });
+});
+
+if (DARK_TOGGLE) {
+  // DARK MODE
+  DARK_TOGGLE.addEventListener("click", (event) => {
+    const BODY = document.querySelector("body");
+    const PARAGRAPH = document.querySelectorAll("p");
+    const SUBHEADLINE = document.querySelectorAll("h3");
+    const MESSAGE = document.querySelector(".dark-mode-toggle__message");
+    LIGHT_BULB.classList.toggle("dark-mode");
+    MESSAGE.classList.toggle("dark-mode");
+    BODY.classList.toggle("dark-mode-bg");
+    PARAGRAPH.forEach((p) => {
+      p.classList.toggle("dark-mode-txt");
+    });
+    SUBHEADLINE.forEach((sub) => {
+      sub.classList.toggle("dark-mode-txt");
+    });
+
+    if (LIGHT_BULB.classList.contains("dark-mode")) {
+      MESSAGE.textContent = "on";
+      SUBHEADLINE.forEach((sub) => {
+        sub.style.textDecoration = "underline";
+      });
+    } else {
+      MESSAGE.textContent = "off";
+      SUBHEADLINE.forEach((sub) => {
+        sub.style.textDecoration = "none";
+      });
+    }
+  });
+}
+FORM.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let question = document.querySelector("#create__question");
+  let answer = document.querySelector("#create__answer");
+  let tags = document.querySelector("#create__tags").value;
+
+  let tagList = tags.split(",");
+
+  let obj = {
+    question: question.value,
+    answer: answer.value,
+    tags: tagList,
+  };
+  formArray.push(obj);
+  console.log(formArray);
+  FORM.reset();
 });
